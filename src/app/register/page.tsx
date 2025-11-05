2970f55af8fca90b7aaa1291e5c0f737eca0becb
Travis Gooden <tgooden@my.bcit.ca>
2025-11-04 20:29:31 -0800

    fix: change student login styles to prevent overriding instructor dashboard fonts

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -82,7 +82,7 @@ function RegisterContent() {
     return (
         <div>
             <div id="ui">
-                <h1>LevelUp EDU</h1>
+                <h1 id="title">LevelUp EDU</h1>
                 <div id="sign-in">
                     {error ?
                         <div style={{textAlign: 'center', color: '#ffffff'}}>
