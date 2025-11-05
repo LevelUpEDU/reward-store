39cffdc3898a0b632553365b3e7d1eebeb489d6c
Travis Gooden <tgooden@my.bcit.ca>
2025-11-04 18:06:31 -0800

    fix: check for page load to fix instructor dashboard crashing on refresh

--- src/app/hooks/useAuth.ts
+++ src/app/hooks/useAuth.ts
@@ -1,10 +1,20 @@
 import {useUser} from '@auth0/nextjs-auth0/client'
 
 export function useAuth() {
-    const {user} = useUser()
+    const {user, isLoading} = useUser()
+
+    if (isLoading || !user) {
+        return {
+            user: null,
+            email: null,
+            name: null,
+            isLoading: true,
+        }
+    }
 
     return {
-        user: user!,
-        email: user!.email as string,
+        user,
+        email: user.email as string,
+        isLoading: false,
     }
 }
