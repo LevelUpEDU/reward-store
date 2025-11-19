4857d4bc7fd0dbc4006eaf257d421063bac2c003
Niki <xwang324@my.bcit.ca>
2025-11-18 20:18:20 -0800

    feat: add ToasterProvider to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './styles/globals.css'
+import {ToasterProvider} from './Toaster'
 
 export const metadata = {
     title: 'LevelUpEDU',
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                {children}
+                <ToasterProvider />
+            </body>
         </html>
     )
 }
