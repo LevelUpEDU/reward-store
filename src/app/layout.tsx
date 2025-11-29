d090598e655d24ef6cff5fcaca56b8b4a091b51c
Travis Gooden <tgooden@my.bcit.ca>
2025-11-28 23:02:06 -0800

    fix: fix missing favicon metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,15 @@ import {ToasterProvider} from './Toaster'
 export const metadata = {
     title: 'LevelUpEDU',
     description: 'Gamified learning platform where education meets adventure',
+    icons: {
+        icon: '/favicon/web-app-manifest-192x192.png',
+        shortcut: '/favicon/web-app-manifest-192x192.png',
+        apple: '/favicon/web-app-manifest-192x192.png',
+        other: {
+            rel: 'apple-touch-icon-precomposed',
+            url: '/favicon/web-app-manifest-192x192.png',
+        },
+    },
 }
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
